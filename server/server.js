8b84df54337364d590f3f870d70e89a75a3d3922
Sannnq <founguesflorin@gmail.com>
2023-03-01 04:57:45 +0100

    Build fix

--- server/server.js
+++ server/server.js
@@ -3,26 +3,26 @@ const server = require("http").createServer();
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB4KIAf78RfZqiJG885Dr5aLFwSM5BWo4g",
-  authDomain: "server-app-hosting.firebaseapp.com",
-  projectId: "server-app-hosting",
-  storageBucket: "server-app-hosting.appspot.com",
-  messagingSenderId: "855551181280",
-  appId: "1:855551181280:web:8deb4c1536b54ef61dab85",
-  measurementId: "G-64GYWP41PM"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB4KIAf78RfZqiJG885Dr5aLFwSM5BWo4g",
+//   authDomain: "server-app-hosting.firebaseapp.com",
+//   projectId: "server-app-hosting",
+//   storageBucket: "server-app-hosting.appspot.com",
+//   messagingSenderId: "855551181280",
+//   appId: "1:855551181280:web:8deb4c1536b54ef61dab85",
+//   measurementId: "G-64GYWP41PM"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
 const io = require("socket.io")(server);
